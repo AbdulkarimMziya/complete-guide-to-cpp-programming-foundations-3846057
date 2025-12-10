79893bb0b21e5ca4bb7efdec6c35ae4c353ee813
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2025-12-10 23:53:30 +0000

    Basic std library, functions

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,29 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Abdul Mziya
+
+#include <iostream> 
+/* 
+<iostrem> is a C++ standard library that contains object and function to print text
+            on the screen and to receive text from keyboard.
+*/
+
+
+/***
+ * Functions: Central part of C++ programming, used to organize and reuse code.
+ * 
+ * Function Components: Include return type, function name, parameters, and function body.
+ * 
+ * Parameters and Arguments: Parameters are placeholders in the function declaration , while arguments are the actual values passed during the function call.
+ * 
+ * Return Types: Specify the type of value a function will return; if no value is returned, the return type is void.
+ */
+
+
+int main(){
+    std::cout << "Hello C++ World" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
+
